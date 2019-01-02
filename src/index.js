7fa296c87769bb9b3c25bbc16be0dbc38e55a2fa
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-03 00:14:00 +0100

    Fix interface

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const { URL } = require('url')
 
 module.exports = ({ text, url }) => {
   const { origin: baseUrl } = new URL(url)
-  const urls = parseCssUrls(text)
-  const normalizedUrl = urls.map(url => normalizeUrl(baseUrl, url))
-  return { urls, normalizedUrl }
+
+  return parseCssUrls(text).map(url => ({
+    url,
+    normalizeUrl: normalizeUrl(baseUrl, url)
+  }))
 }
