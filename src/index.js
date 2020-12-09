4b6ee175bbc69a20b68c9b53530a43d6edf9ab3c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-12-09 10:37:13 +0100

    build: improve implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 'use strict'
 
 const { normalizeUrl } = require('@metascraper/helpers')
-const parseCssUrls = require('css-url-parser')
+const cssUrl = require('css-url-regex')
+const execall = require('execall')
 const { URL } = require('url')
 
 module.exports = ({ text, url }) => {
   const { origin: baseUrl } = new URL(url)
 
-  return parseCssUrls(text).map(url => ({
+  return Array.from(
+    execall(cssUrl(), text).reduce((acc, match) => {
+      match.subMatches.forEach(match => acc.add(match.replace(/['"]+/g, '')))
+      return acc
+    }, new Set())
+  ).map(url => ({
     url,
     normalizedUrl: normalizeUrl(baseUrl, url)
   }))
--- test/index.js
+++ test/index.js
@@ -6,20 +6,24 @@ const fs = require('fs')
 
 const cssUrls = require('..')
 
-test('inline css', t => {
+test('inline css with double quotes', t => {
   const url = 'https://kikobeats.com'
   const text =
     '<div class="db w-100 h0 pb50 bg-center cover mb2" style="background-image: url("/images/windtoday.png"); will-change: transform; transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1);" data-tilt="" data-tilt-max="10" data-tilt-glare="" data-tilt-max-glare="0.8" data-tilt-reverse="true"><div class="js-tilt-glare" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden;"><div class="js-tilt-glare-inner" style="position: absolute; top: 50%; left: 50%; pointer-events: none; background-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 100%); width: 1104px; height: 1104px; transform: rotate(180deg) translate(-50%, -50%); transform-origin: 0% 0% 0px; opacity: 0;"></div></div></div>'
   t.snapshot(cssUrls({ url, text }))
 })
 
+test('inline css with single quotes', t => {
+  const url = 'https://kikobeats.com'
+  const text =
+    '<div class="db w-100 h0 pb50 bg-center cover mb2" style="background-image: url(\'/images/windtoday.png\'); will-change: transform; transform: perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1);" data-tilt="" data-tilt-max="10" data-tilt-glare="" data-tilt-max-glare="0.8" data-tilt-reverse="true"><div class="js-tilt-glare" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden;"><div class="js-tilt-glare-inner" style="position: absolute; top: 50%; left: 50%; pointer-events: none; background-image: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgb(255, 255, 255) 100%); width: 1104px; height: 1104px; transform: rotate(180deg) translate(-50%, -50%); transform-origin: 0% 0% 0px; opacity: 0;"></div></div></div>'
+  t.snapshot(cssUrls({ url, text }))
+})
+
 test('from stylesheet', async t => {
   const url = 'https://elenatorro.github.io'
 
-  const text = fs.readFileSync(
-    path.resolve(__dirname, 'fixtures/style.css'),
-    'utf8'
-  )
+  const text = fs.readFileSync(path.resolve(__dirname, 'fixtures/style.css'), 'utf8')
 
   t.snapshot(cssUrls({ url, text }))
 })
