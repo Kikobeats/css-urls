ee23af470f06932bf9c2cddd0c9592d25e9520df
Jose Francisco 'Kiko' Verdú Gambín <josefrancisco.verdu@gmail.com>
2026-03-05 17:34:08 +0100

    perf: use stable-regex (#47)

--- README.md
+++ README.md
@@ -57,4 +57,4 @@ The target URL(s) for extracting URLs referenced.
 **css-urls** © [Kiko Beats](https://kikobeats.com), released under the [MIT](https://github.com/Kikobeats/css-urls/blob/master/LICENSE.md) License.<br>
 Authored and maintained by Kiko Beats with help from [contributors](https://github.com/Kikobeats/css-urls/contributors).
 
-> [kikobeats.com](https://kikobeats.com) · GitHub [Kiko Beats](https://github.com/Kikobeats) · Twitter [@Kikobeats](https://twitter.com/Kikobeats)
+> [kikobeats.com](https://kikobeats.com) · GitHub [Kiko Beats](https://github.com/Kikobeats) · X [@Kikobeats](https://x.com/Kikobeats)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 'use strict'
 
 const { normalizeUrl } = require('@metascraper/helpers')
-const cssUrl = require('css-url-regex')
 const execall = require('execall')
+const stableRegex = require('stable-regex')
+const urlRegex = require('url-regex-safe')
 const { URL } = require('url')
+
+const CSS_URL_FUNCTION_PATTERN = /url\((.*?)\)/gi
+const ABSOLUTE_URL_PATTERN = urlRegex({ exact: true, strict: false })
+const normalizeMatch = input => input.replace(/['"]+/g, '').trim()
+const toCssUrlValue = input => {
+  const value = normalizeMatch(input)
+  if (!stableRegex(ABSOLUTE_URL_PATTERN, value)) return value
+  return value
+}
 
 module.exports = ({ text, url }) => {
   const { origin: baseUrl } = new URL(url)
+  if (!stableRegex(CSS_URL_FUNCTION_PATTERN, text)) return []
+  CSS_URL_FUNCTION_PATTERN.lastIndex = 0
 
   return Array.from(
-    execall(cssUrl(), text).reduce((acc, match) => {
-      match.subMatches.forEach(match => acc.add(match.replace(/['"]+/g, '')))
+    execall(CSS_URL_FUNCTION_PATTERN, text).reduce((acc, match) => {
+      match.subMatches.forEach(match => acc.add(toCssUrlValue(match)))
       return acc
     }, new Set())
   ).map(url => ({
